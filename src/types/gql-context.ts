f5bfe8f305c440f1f15a11985843b7a77091ea60
vini00784 <vo70346@gmail.com>
2024-07-12 19:44:56 -0300

    feat: create context to manipulate user login

---
+++ src/types/gql-context.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+import { User } from "src/graphql/entities/user";
+import { UserRoleEnum } from "./user-role";
+
+export interface GqlContext {
+    req: Request;
+    res: Response;
+    user?: Omit<User, "password"> | null & { role: keyof typeof UserRoleEnum };
+}
