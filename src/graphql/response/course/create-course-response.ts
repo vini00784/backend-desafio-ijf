eac92e2c3cf6aa8881b7c92e58b85858f926bb9b
vini00784 <vo70346@gmail.com>
2024-07-12 19:38:56 -0300

    feat: add response to create course mutation

---
+++ src/graphql/response/course/create-course-response.ts
@@ -0,0 +1,11 @@
+import { Field, ID, ObjectType } from "@nestjs/graphql";
+import { Course } from "src/graphql/entities/course";
+
+@ObjectType()
+export class CreateCourseResponse {
+    @Field(() => ID, { nullable: false })
+    id: Course["id"];
+
+    @Field(() => Date, { nullable: false })
+    createdAt: Date;
+}
