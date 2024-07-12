82fd083c7cd4bbecfef7e2884475304afb900100
vini00784 <vo70346@gmail.com>
2024-07-12 19:46:12 -0300

    feat: add function to extract user from headers

--- src/middleware/jwt.ts
+++ src/middleware/jwt.ts
@@ -1,17 +1,22 @@
 import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { JwtService } from "@nestjs/jwt";
+import prisma from "src/database/prisma";
+import { User } from "src/graphql/entities/user";
+import { GqlContext } from "src/types/gql-context";
 import { UserRoleEnum } from "src/types/user-role";
 
 @Injectable()
 export class JwtMiddleware {
     constructor(
-        private readonly jwtService: JwtService
+        private readonly jwtService: JwtService,
+        private readonly configService: ConfigService
     ) {}
 
     signIn(userId: string, userRole: keyof typeof UserRoleEnum) {
         const payload = {
             sub: userId,
-            userRole: userRole
+            role: userRole
         };
 
         const token = this.jwtService.sign(payload);
@@ -19,5 +24,59 @@ export class JwtMiddleware {
         return {
             token
         }
+    }
+
+    extractUserFromHeaders = async (
+        headers: GqlContext["req"]["headers"]
+    ): Promise<Omit<User, "password"> & { role: keyof typeof UserRoleEnum }> => {
+        const { authorization } = headers;
+        
+        if(!authorization) return undefined;
+        
+        const token = authorization.split(' ')[1];
+        const { sub, role } = this.#getPayloadByToken(token);
+
+        if(role === "teacher") {
+            const foundedTeacher = await prisma.teacher.findUnique({
+                where: {
+                    id: sub
+                }
+            });
+
+            return {
+                id: foundedTeacher.id,
+                name: foundedTeacher.name,
+                createdAt: foundedTeacher.createdAt,
+                username: foundedTeacher.username,
+                role: "teacher"
+            }
+        } else if(role === "student") {
+            const foundedStudent = await prisma.student.findUnique({
+                where: {
+                    id: sub
+                }
+            })
+
+            return {
+                id: foundedStudent.id,
+                name: foundedStudent.name,
+                createdAt: foundedStudent.createdAt,
+                username: foundedStudent.username,
+                role: "student"
+            }
+        }
+    }
+
+    #getPayloadByToken(
+        token: string
+    ) {
+        const payload = this.jwtService.verify(token, {
+            secret: this.configService.get('JWT_SECRET')
+        });
+
+        return {
+            sub: payload.sub,
+            role: payload.role
+        };
     }
 }
