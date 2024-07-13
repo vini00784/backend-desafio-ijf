34be10fbab1b2f0a4140551af1972bd8bfd5631a
vini00784 <vo70346@gmail.com>
2024-07-13 01:08:51 -0300

    feat: add response to remove student of course mutation

---
+++ src/graphql/response/course/remove-student-response.ts
@@ -0,0 +1,7 @@
+import { Field, ObjectType } from "@nestjs/graphql";
+
+@ObjectType()
+export class RemoveStudentOfCourseResponse {
+    @Field(() => String, { nullable: false })
+    message: string;
+}
