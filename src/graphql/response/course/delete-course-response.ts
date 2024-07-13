9f374863d5ea684905c19acd8fec0604022676a0
vini00784 <vo70346@gmail.com>
2024-07-12 21:53:57 -0300

    feat: add response to delete course mutation

---
+++ src/graphql/response/course/delete-course-response.ts
@@ -0,0 +1,11 @@
+import { Field, ID, ObjectType } from "@nestjs/graphql";
+import { Course } from "@prisma/client";
+
+@ObjectType()
+export class DeleteCourseResponse {
+    @Field(() => ID, { nullable: false })
+    id: Course["id"];
+
+    @Field(() => Date, { nullable: false })
+    deletedAt: Date;
+}
