24cdf2943eac682b9b248d8558f69069e8b6e7cb
vini00784 <vo70346@gmail.com>
2024-07-14 19:19:09 -0300

    feat: add input to watch lesson mutation

---
+++ src/graphql/input/lesson/watch-lesson.input.ts
@@ -0,0 +1,8 @@
+import { Field, ID, InputType } from "@nestjs/graphql";
+import { Lesson } from "src/graphql/entities/lesson";
+
+@InputType()
+export class WatchLessonInput {
+    @Field(() => ID, { nullable: false })
+    lessonId: Lesson["id"];
+}
