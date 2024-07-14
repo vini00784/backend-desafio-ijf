0bacba27b4647be5e8fe4c9326bfdb0d607d3dd0
vini00784 <vo70346@gmail.com>
2024-07-14 20:15:58 -0300

    feat: add response to create user mutation

---
+++ src/graphql/response/auth/create-user-response.ts
@@ -0,0 +1,14 @@
+import { Field, ID, ObjectType } from "@nestjs/graphql";
+import { User } from "src/graphql/entities/user";
+
+@ObjectType()
+export class CreateUserResponse {
+    @Field(() => ID, { nullable: false })
+    id: User["id"];
+
+    @Field(() => String, { nullable: false })
+    username: string;
+
+    @Field(() => Date, { nullable: false })
+    createdAt: Date;
+}
