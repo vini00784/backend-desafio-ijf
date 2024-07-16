7bb9af1b37fcbc7a63b6a965894e0052ea9b01b4
vini00784 <vo70346@gmail.com>
2024-07-15 21:27:30 -0300

    fix: change attribute type

--- src/domain/entities/course.ts
+++ src/domain/entities/course.ts
@@ -49,7 +49,7 @@ export class Course extends BaseEntity<CourseProps> {
         this.props.students.splice(foundedStudent, 1);
     }
 
-    edit(data: Omit<Course, "teacher">) {
+    edit(data: Omit<CourseProps, "teacher">) {
         this.props = {
             ...this.props,
             ...data
